32a301e6ea5f0d15729ef2aac763aee27315eafe
mynameiskorea86-hue <mynameiskorea86@gmail.com>
2026-08-06 13:25:06 +0000

    Fix mobile cache busting for stale page reloads

--- tests/cache-busting.test.js
+++ tests/cache-busting.test.js
@@ -6,5 +6,7 @@ const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
 
 assert.match(html, /<link rel="stylesheet" href="styles\.css\?v=/, 'styles.css should use a versioned asset URL');
 assert.match(html, /<script src="script\.js\?v=/, 'script.js should use a versioned asset URL');
+assert.match(html, /const APP_VERSION =/, 'app version should be defined in the page bootstrap');
+assert.match(html, /params\.set\('v'/, 'page bootstrap should force a versioned reload to bypass stale mobile cache');
 
 console.log('cache busting test passed');
