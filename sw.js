da077d8b3b013e64ea4725641c9d6faa3725c5a6
mynameiskorea86-hue <mynameiskorea86@gmail.com>
2026-08-09 21:58:22 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = "r01-pwa-v1";
 
+const FILES_TO_CACHE = [
+  "/R01/",
+  "/R01/index.html",
+  "/R01/styles.css",
+  "/R01/script.js",
+  "/R01/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
